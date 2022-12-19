3aaae4c39651226210577fc1aa4ee76a622cefa0
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-12-19 21:46:37 +0100

    :sparkles: restructring with dask

---
+++ bye_splits/data_handle/data_handle.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+
+_all_ = [ 'data_handle' ]
+
+import os
+from pathlib import Path
+import sys
+parent_dir = os.path.abspath(__file__ + 2 * '/..')
+sys.path.insert(0, parent_dir)
+
+import yaml
+from data_handle.geometry import GeometryData
+from data_handle.event import EventData
+from utils import params
+
+class EventDataParticle:
+    def __init__(self, particles, tag):
+        assert particles in ('photons', 'electrons')
+        self.particles = particles
+        self.tag = self.particles + '_' + tag
+        with open(params.viz_kw['CfgEventPath'], 'r') as afile:
+            self.config = yaml.safe_load(afile)
+
+        in_name = '_'.join(('skim', self.particles, '0PU_bc_stc_hadd.root'))    
+        default_events = self.config['defaultEvents'][self.particles]
+        self.data = EventData(in_name, self.tag, default_events)
+
+    def provide_event(self, event):
+        return self.data.provide_event(event)
