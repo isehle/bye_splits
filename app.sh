c7da3cfe2683071819b66cec665e858779ea04b6
bfontana <20703947+b-fontana@users.noreply.github.com>
2023-01-06 18:14:02 +0100

    :construction: prepare bokeh+plotly+flask S2I deployment

--- app.sh
+++ app.sh
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 #! /bin/bash
 
 python bye_splits/scripts/cl_app.py
 
 #--allow-websocket-origin=bye-splits-app-hgcal-cl-size-studies.app.cern.ch
+=======
+python bye_splits/plot/join/app.py --flask_port 8010 --bokeh_port 8008
+#bokeh serve bye_splits/plot/display/ --address 0.0.0.0 --port 8080 --allow-websocket-origin=viz2-hgcal-event-display.app.cern.ch
+>>>>>>> :construction: prepare bokeh+plotly+flask S2I deployment
