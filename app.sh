11c77f6df8d028db0b19c41a7aa2c24504cc943d
Isaac Ehle <itehle@gmail.com>
2023-01-26 17:32:44 +0100

    Modified app.sh to call the DashApp

---
+++ app.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+python bye_splits/scripts/cl_app.py
+#--allow-websocket-origin=hgcal-cl-size-studies.app.cern.ch
--- bye_splits/scripts/cl_app.py
+++ bye_splits/scripts/cl_app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import sys
+
 from dash import Dash, html, dcc
 import dash
 
